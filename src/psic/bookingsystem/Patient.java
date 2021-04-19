1bf38f8da7193d9c09200c290ade7ba076eb9971
Hart Praise <hartpraise@gmail.com>
2021-04-19 11:37:03 +0100

    Finished Patient Class and unit tests

---
+++ src/psic/bookingsystem/Patient.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psic.bookingsystem;
+
+/**
+ *
+ * @author Hart
+ */
+public class Patient {
+    private int id;
+    private String fullName;
+    private String address;
+    private String phoneNumber;
+    
+    public Patient(int id, String fullName, String address, String phoneNumber) {
+        this.setId(id);
+        this.setFullName(fullName);
+        this.setAddress(address);
+        this.setPhoneNumber(phoneNumber);
+    }
+    public Patient() {
+        
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the fullName
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * @param fullName the fullName to set
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+}
